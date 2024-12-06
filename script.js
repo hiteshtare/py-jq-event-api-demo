1c81050b8d7e98c1598426a40f2ce56e08a8f051
hiteshtare <hiteshtare@gmail.com>
2024-12-06 13:03:04 +0530

    Thanks PY - Build table with row & column spans

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log('Hello!');
+
+var data = [{
+    "user_data": [{
+      "amount": 2550,
+      "time1": "2017/04/05",
+      "time2": "2017/04/06"
+    }, {
+      "amount": 2550,
+      "time1": "2017/04/05",
+      "time2": "2017/04/06"
+    }],
+    "name": "hulk"
+  }, {
+    "user_data": [ {
+      "amount": 2125,
+      "time1": "2017/04/05",
+      "time2": "2017/04/06"
+    }, {
+      "amount": 1700,
+      "time1": "2017/04/05",
+      "time2": "2017/04/06"
+    }, {
+      "amount": 1700,
+      "time1": "2017/04/05",
+      "time2": "2017/04/06"
+    }, {
+      "amount": 2125,
+      "time1": "2017/04/05",
+      "time2": "2017/04/06"
+    }],
+    "name": "gomu"
+  }];
+  
+  var tableStr = '';
+  data.forEach((obj) => {
+      var userData = obj.user_data;
+      var total = 0;
+     
+      userData.forEach((o, index) => {
+        tableStr += '<tr>' + (index == 0 ? '<td rowspan="' + userData.length + '">' + obj.name + '</td>' : '') + '<td>'+o.time1+'</td><td>'+o.time2+'</td><td>'+o.amount+'</td></tr>';
+         total += o.amount;
+     });
+     tableStr += '<tr><td colspan="3">Total</td><td>' + total + '</td></tr>';
+  });
+  $('#user tbody').html(tableStr);
