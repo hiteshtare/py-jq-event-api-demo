03950c5f8cedc94957510a21f9a8ac377d30da36
hiteshtare <hiteshtare@gmail.com>
2024-12-06 17:40:13 +0530

    Dynamic calc for colspan & rowspan

--- script.js
+++ script.js
@@ -7,41 +7,29 @@ var result = [
         "event_date": "2024-12-25",
         "event_desc": "Merry Christmas",
         "event_image": "https://yssofindia.org/wp-content/uploads/2024/09/Jesus-Christ-Featured-Image-Ranchi-Ashram-Homepage-Events-Section-300x300.jpg",
-        "schedule_id": 1,
-        "schedule_date": "2024-12-25",
-        "schedule_starttime": "2024-01-05T06:30:00+05:30",
-        "schedule_endtime": "2024-01-05T08:00:00+05:30",
-        "schedule_name": "Morning Meditation",
-        "colspan": 1,
-        "rowspan": 3
-    },
-    {
-        "event_id": 2,
-        "event_title": "Christmas",
-        "event_date": "2024-12-25",
-        "event_desc": "Merry Christmas",
-        "event_image": "https://yssofindia.org/wp-content/uploads/2024/09/Jesus-Christ-Featured-Image-Ranchi-Ashram-Homepage-Events-Section-300x300.jpg",
-        "schedule_id": 2,
-        "schedule_date": "2024-12-25",
-        "schedule_starttime": "2024-01-05T09:30:00+05:30",
-        "schedule_endtime": "2024-01-05T11:30:00+05:30",
-        "schedule_name": "Guru Puja",
-        "colspan": 1,
-        "rowspan": 3
-    },
-    {
-        "event_id": 2,
-        "event_title": "Christmas",
-        "event_date": "2024-12-25",
-        "event_desc": "Merry Christmas",
-        "event_image": "https://yssofindia.org/wp-content/uploads/2024/09/Jesus-Christ-Featured-Image-Ranchi-Ashram-Homepage-Events-Section-300x300.jpg",
-        "schedule_id": 3,
-        "schedule_date": "2024-12-25",
-        "schedule_starttime": "2024-01-05T18:00:00+05:30",
-        "schedule_endtime": "2024-01-05T21:00:00+05:30",
-        "schedule_name": "Evening Meditation",
-        "colspan": 1,
-        "rowspan": 3
+        "schedule": [
+            {
+                "schedule_id": 1,
+                "schedule_date": "2024-12-25",
+                "schedule_starttime": "2024-01-05T06:30:00+05:30",
+                "schedule_endtime": "2024-01-05T08:00:00+05:30",
+                "schedule_name": "Morning Meditation",
+            },
+            {
+                "schedule_id": 2,
+                "schedule_date": "2024-12-25",
+                "schedule_starttime": "2024-01-05T09:30:00+05:30",
+                "schedule_endtime": "2024-01-05T11:30:00+05:30",
+                "schedule_name": "Guru Puja",
+            },
+            {
+                "schedule_id": 3,
+                "schedule_date": "2024-12-25",
+                "schedule_starttime": "2024-01-05T18:00:00+05:30",
+                "schedule_endtime": "2024-01-05T21:00:00+05:30",
+                "schedule_name": "Evening Meditation",
+            }
+        ]
     },
     {
         "event_id": 3,
@@ -49,13 +37,17 @@ var result = [
         "event_date": "2024-12-31",
         "event_desc": "Happy New Year",
         "event_image": "https://yssofindia.org/wp-content/uploads/2024/01/YSS-Ranchi-Ashram-Diwali-2023-Featured-Image-600x450.jpg",
-        "schedule_id": 3,
-        "schedule_date": "2024-12-31",
-        "schedule_starttime": "2024-12-31T23:30:00+05:30",
-        "schedule_endtime": "2025-01-01T00:30:00+05:30",
-        "schedule_name": "Special New Years Eve Meditation",
-        "colspan": 1,
-        "rowspan": 1
+        "schedule": [
+            {
+                "schedule_id": 1,
+                "schedule_date": "2024-12-31",
+                "schedule_starttime": "2024-12-31T23:30:00+05:30",
+                "schedule_endtime": "2025-01-01T00:30:00+05:30",
+                "schedule_name": "Special New Years Eve Meditation",
+                "colspan": 1,
+                "rowspan": 1
+            }
+        ]
     },
     {
         "event_id": 5,
@@ -63,27 +55,27 @@ var result = [
         "event_date": "2025-01-05",
         "event_desc": "The celebrations of the 124th Birth Anniversary of Sri Sri Paramahansa Yogananda (1893-1952), author of the spiritual classic ‘Autobiography of a Yogi’, and the centenary of the Yogoda Satsanga Society of India (YSS) took place on 5th Jan in Ranchi",
         "event_image": "https://yssofindia.org/wp-content/uploads/2024/09/Paramahansa-Yogananda-Standard-Image-Ranchi-Ashram-Homepage-Events-Section-300x300.jpg",
-        "schedule_id": 1,
-        "schedule_date": "2025-01-05",
-        "schedule_starttime": "2025-01-05T06:40:00+05:30",
-        "schedule_endtime": "2025-01-05T08:00:00+05:30",
-        "schedule_name": "Morning Meditation",
-        "colspan": 1,
-        "rowspan": 2
-    },
-    {
-        "event_id": 5,
-        "event_title": "Gurudeva’s Janmotsav",
-        "event_date": "2025-01-05",
-        "event_desc": "The celebrations of the 124th Birth Anniversary of Sri Sri Paramahansa Yogananda (1893-1952), author of the spiritual classic ‘Autobiography of a Yogi’, and the centenary of the Yogoda Satsanga Society of India (YSS) took place on 5th Jan in Ranchi",
-        "event_image": "https://yssofindia.org/wp-content/uploads/2024/09/Paramahansa-Yogananda-Standard-Image-Ranchi-Ashram-Homepage-Events-Section-300x300.jpg",
-        "schedule_id": 2,
-        "schedule_date": "2025-01-05",
-        "schedule_starttime": "2025-01-05T18:00:00+05:30",
-        "schedule_endtime": "2025-01-05T19:40:00+05:30",
-        "schedule_name": "Evening Meditation",
-        "colspan": 1,
-        "rowspan": 2
+
+        "schedule": [
+            {
+                "schedule_id": 1,
+                "schedule_date": "2025-01-05",
+                "schedule_starttime": "2025-01-05T06:40:00+05:30",
+                "schedule_endtime": "2025-01-05T08:00:00+05:30",
+                "schedule_name": "Morning Meditation",
+                "colspan": 1,
+                "rowspan": 2
+            },
+            {
+                "schedule_id": 2,
+                "schedule_date": "2025-01-05",
+                "schedule_starttime": "2025-01-05T18:00:00+05:30",
+                "schedule_endtime": "2025-01-05T19:40:00+05:30",
+                "schedule_name": "Evening Meditation",
+                "colspan": 1,
+                "rowspan": 2
+            }
+        ]
     }
 ];
 
@@ -121,19 +113,26 @@ var data = [{
 
 var tableStr = '';
 result.forEach((obj) => {
-    tableStr += '<tr><td colspan="' + obj.colspan + '">' + `<img class="heroimage" src='${obj.event_image}'></img>` + '</td>' + '<td>' + obj.event_date + '</td><td>' + obj.event_title + '</td><td>' + obj.schedule_starttime+ '</td></tr>';
+    const colImage = `<td rowspan="${obj.schedule.length}"><img class="heroimage" src="${obj.event_image}"></img></td>`;
+    const colDate = `<td rowspan="${obj.schedule.length}">${obj.event_date}</td>`;
+    const colTitle = `<td rowspan="${obj.schedule.length}">${obj.event_title}</td>`;
+
+    let colSchedule = ``;
+    let tableRow = ``;
+    obj.schedule.forEach((item,index) => {
+
+        colSchedule = `<td style="vertical-align: middle !important">
+        <p style="text-align: center">${item.schedule_starttime} to ${item.schedule_endtime}</p>
+
+        <p style="text-align: center">${item.schedule_name}</p>
+        </td>`;
+        if(index == 0) {
+            tableRow = `<tr>${colImage}${colDate}${colTitle}${colSchedule}</tr>`;
+        }
+        else {
+            tableRow = `<tr>${colSchedule}</tr>`;
+        }
+        tableStr += tableRow;
+    });
 });
 $('#schedule tbody').html(tableStr);
-
-var tableStr = '';
-data.forEach((obj) => {
-    var userData = obj.user_data;
-    var total = 0;
-
-    userData.forEach((o, index) => {
-        tableStr += '<tr>' + (index == 0 ? '<td rowspan="' + userData.length + '">' + obj.name + '</td>' : '') + '<td>' + o.time1 + '</td><td>' + o.time2 + '</td><td>' + o.amount + '</td></tr>';
-        total += o.amount;
-    });
-    tableStr += '<tr><td colspan="3">Total</td><td>' + total + '</td></tr>';
-});
-$('#user tbody').html(tableStr);
